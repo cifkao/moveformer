1fef583c3500060c1cf4bcc773852d23c4a582a3
Ondřej Cífka <cifkao@users.noreply.github.com>
2022-09-25 11:30:29 +0200

    Add TransformedDataset

--- geo_transformers/data_loading.py
+++ geo_transformers/data_loading.py
@@ -2,9 +2,22 @@ import abc
 from typing import Any, Dict
 
 import numpy as np
+import torch
 
 
 class DataProcessor:
     @abc.abstractmethod
     def encode(self, example: Dict[str, Any]) -> Dict[str, np.ndarray]:
         pass
+
+
+class TransformedDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset_raw, preprocess_fn):
+        self.dataset_raw = dataset_raw
+        self.preprocess_fn = preprocess_fn
+
+    def __getitem__(self, idx):
+        return self.preprocess_fn(self.dataset_raw[idx])
+
+    def __len__(self):
+        return len(self.dataset_raw)
